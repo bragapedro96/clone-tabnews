0bf19c0f359424132bb613459678988234f9e548
bragapedro96 <106757103+bragapedro96@users.noreply.github.com>
2026-07-07 11:45:37 +0000

    Reescreve a frase inicial da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Essa é a página inicial do projeto</h1>
+    return <h1>Essa é a página inicial do projeto :) </h1>
 }
 
-export default Home;
+export default Home;git push --force
